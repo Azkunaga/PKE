2c222cc967b5eabd8d8293da9599bba26e077002
Azkunaga <aritzazkunaga@users.noreply.github.com>
2021-12-13 11:47:33 +0100

    boostrap sartuta

--- php/Credits.php
+++ php/Credits.php
@@ -10,7 +10,7 @@
 	}
 
 	.imgizq{
-		float:left; /*ara ponerlo a la izquierda */
+		float:left; /*para ponerlo a la izquierda */
 		margin:20px; /* un margen cualquiera */
 	}
 	.imgizq img, .imgder img{
@@ -22,8 +22,9 @@
 		font-weight:bold; 
 		font-size: 14px; 
 	}
-
   </style>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
   <?php include '../php/Menus.php' ?>
