3ac2facd7ed931aa4845836760999157eed4d43a
Azkunaga <aritzazkunaga@users.noreply.github.com>
2021-12-13 11:04:09 +0100

    fitxategiak ezabatuta

--- php/ClientVerifyEnrollment.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$soapclient = new SoapClient('http://ehusw.es/rosa/webZerbitzuak/egiaztatuMatrikula.php?wsdl');
-if (isset($_POST['eposta'])){
-	echo $soapclient->egiaztatuE($_POST['eposta']); 
-}
-?>
--- php/Segurtasuna.php
+++ php/Segurtasuna.php
@@ -2,7 +2,7 @@
 	//sesio hasiera
 	session_start();
 	// IKASLEA KAUTOTURIK DAGOELA EGIAZTATU
-	if (!isset($_SESSION['email']) || $_SESSION['erabMota']!="irakaslea" || $_SESSION['email']=="admin@ehu.es") {
+	if (!isset($_SESSION['email'])) {
 		// existitzen ez bada, berriro kautotzera bidaltzen dut
 		header("Location: Layout.php");
 		//gainera, script-atik irtetzen gara
